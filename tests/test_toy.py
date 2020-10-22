9e48a6643509b6dc5c561fbeba8c98b8c0f269d5
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-10-22 14:59:41 +0200

    Test for toy experiment.

---
+++ tests/test_toy.py
@@ -0,0 +1,20 @@
+from pytorch_lightning import Trainer, seed_everything
+from sggm.data.toy.datamodule import ToyDataModule
+from sggm.regression_model import fit_prior, Regressor
+
+
+def test_lit_classifier():
+    seed_everything(1234)
+    prior_parameters = fit_prior()
+    model = Regressor(
+        input_dim=1,
+        hidden_dim=50,
+        prior_α=prior_parameters[0],
+        prior_β=prior_parameters[1],
+    )
+    datamodule = ToyDataModule(128)
+    trainer = Trainer(max_epochs=5)
+    trainer.fit(model, datamodule)
+
+    results = trainer.test()
+    assert results[0]["eval_loss"] < 4
