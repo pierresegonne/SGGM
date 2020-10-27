7d6fb12d97c496539a12f1b759ff4dbf0f885fd5
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-10-27 14:02:45 +0100

    Fix package name.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SGGN",
+    name="SGGM",
     version="0.0.0",
     description="Scalable Geometrical Generative Models",
     author="Pierre Segonne",
