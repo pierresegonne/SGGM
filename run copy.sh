c6e76ae020d088636aad36416fb940076db24de8
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-10-27 13:33:52 +0100

    investigation warning run.

--- run copy.sh
+++ run copy.sh
@@ -35,6 +35,6 @@ module load cudnn/v7.6.5.32-prod-cuda-10.2
 python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
-cd sggm/ && python experiment.py --experiments_config configs/benchmarks.yml
+echo "all done"
 
 
