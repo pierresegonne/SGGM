7d3eb99bdaa6ffd65502b0c06dfe9a9c2661a8c8
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-11-02 09:42:21 +0100

    Val freq experiment.

--- run.sh
+++ run.sh
@@ -34,4 +34,4 @@ module load cudnn/v7.6.5.32-prod-cuda-10.2
 python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
-cd sggm/ && python experiment.py --experiments_config configs/benchmarks.yml
+cd sggm/ && python experiment.py --experiments_config configs/val_update_frequency.yml
