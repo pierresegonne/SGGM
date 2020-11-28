9628e7b805eb252987f2d6f5544189e13b4fc3b7
Pierre Segonne <pierre.segonne@student.ecp.fr>
2020-11-28 16:14:38 +0100

    Fix test.

--- tests/test_toy.py
+++ tests/test_toy.py
@@ -1,16 +1,13 @@
 from pytorch_lightning import Trainer, seed_everything
 from sggm.data.toy.datamodule import ToyDataModule
 from sggm.regression_model import Regressor
-from sggm.definitions import EVAL_LOSS, TEST_LOSS
+from sggm.definitions import EVAL_LOSS, F_SIGMOID, TEST_LOSS
 
 
 def test_toy():
 
     seed_everything(1234)
-    model = Regressor(
-        input_dim=1,
-        hidden_dim=50,
-    )
+    model = Regressor(input_dim=1, hidden_dim=50, activation_function=F_SIGMOID)
     datamodule = ToyDataModule(128, 0)
     trainer = Trainer(max_epochs=5)
     trainer.fit(model, datamodule)
