034534ffe60144bc598975a392cc1c6e79cf47c7
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-01-12 19:18:55 +0100

    Correct config name.

--- run.sh
+++ run.sh
@@ -37,4 +37,4 @@ python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
 export PYTHONIOENCODING=utf8
-cd sggm/ && python experiment.py --experiments_config configs/benchmark_shifted.yml --gpus -1
+cd sggm/ && python experiment.py --experiments_config configs/benchmarks_shifted.yml --gpus -1
