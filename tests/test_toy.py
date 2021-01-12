7b4174ee3a2ed571de7a992c95f7741a9f97a23d
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-01-12 18:21:45 +0100

    Fix default mixture proportion in/out.

--- tests/test_toy.py
+++ tests/test_toy.py
@@ -9,7 +9,7 @@ def test_toy():
     seed_everything(123)
     model = VariationalRegressor(input_dim=1, hidden_dim=50, activation=F_SIGMOID)
     datamodule = ToyDataModule(128, 0)
-    trainer = Trainer(max_epochs=10)
+    trainer = Trainer(max_epochs=5)
     trainer.fit(model, datamodule)
 
     results = trainer.test()
