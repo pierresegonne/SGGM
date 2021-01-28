942bdc869483fae7a673d40aeb6c063b2eceb029
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-01-28 15:55:07 +0100

    Exit shift inv mode.

--- sggm/experiment.py
+++ sggm/experiment.py
@@ -416,7 +416,7 @@ def cli_main():
             datamodule = experiment.datamodule
             datamodule.setup()
 
-            shift_investigation = True
+            shift_investigation = False
             if shift_investigation:
                 print(
                     f"{len(datamodule.train_dataset)};{len(datamodule.val_dataset)};{len(datamodule.test_dataset)}"
