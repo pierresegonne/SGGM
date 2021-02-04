05ae38ef1f0aa96ac1586f0d0d52c9c110755e91
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-02-04 11:11:13 +0100

    Same visualisation for analysis and callback.

--- sggm/callbacks/img_generated_saver.py
+++ sggm/callbacks/img_generated_saver.py
@@ -1,6 +1,10 @@
+import io
+import matplotlib.pyplot as plt
 import pytorch_lightning as pl
+import torch
 import torchvision
 
+from PIL import Image
 from pytorch_lightning import Callback, Trainer, LightningModule
 
 from sggm.vae_model_helper import batch_reshape
@@ -8,6 +12,29 @@ from sggm.vae_model_helper import batch_reshape
 """
 Callback to log generated images
 """
+
+
+def plot_to_image(fig):
+    """Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(fig)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    # image = tf.image.decode_png(buf.getvalue(), channels=4)
+    image = Image.open(buf).convert("RGB")
+    image = torchvision.transforms.ToTensor()(image)
+    return image
+
+
+def disable_ticks(ax):
+    ax.tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+    ax.tick_params(axis="y", which="both", left=False, labelleft=False)
+    return ax
 
 
 class IMGGeneratedSaver(pl.callbacks.Callback):
@@ -25,25 +52,48 @@ class IMGGeneratedSaver(pl.callbacks.Callback):
         if (trainer.current_epoch == 0) | (
             trainer.current_epoch % self.save_every_epochs == 0
         ):
-            x, y = next(iter(pl_module.test_dataloader()))
+            x, y = next(iter(pl_module.val_dataloader()))
             x, y = x.to(pl_module.device), y.to(pl_module.device)
-            x_hat, p_x = pl_module(x)
+            with torch.no_grad():
+                x_hat, p_x = pl_module(x)
 
-            # Show only mean
-            x = 1 - x
-            x_hat = 1 - x_hat
-            x_mean = 1 - batch_reshape(p_x.mean, pl_module.input_dims)
-            x_var = 1 - batch_reshape(p_x.variance, pl_module.input_dims)
+                mean_error = torch.nn.functional.mse_loss(
+                    batch_reshape(p_x.mean[0], pl_module.input_dims), x
+                )
 
-            img_list = (
-                [x[i] for i in range(self.num_images)]
-                + [x_mean[i] for i in range(self.num_images)]
-                + [x_var[i] for i in range(self.num_images)]
-                + [x_hat[i] for i in range(self.num_images)]
+            print(mean_error)
+
+            x_hat = x_hat
+            x_mean = batch_reshape(p_x.mean, pl_module.input_dims)
+            x_var = batch_reshape(p_x.variance, pl_module.input_dims)
+
+            fig = plt.figure()
+            n_display = 4
+            gs = fig.add_gridspec(
+                4, n_display, width_ratios=[1] * n_display, height_ratios=[1, 1, 1, 1]
             )
-            grid = torchvision.utils.make_grid(img_list, nrow=self.num_images)
+            gs.update(wspace=0, hspace=0)
+            for n in range(n_display):
+                for k in range(4):
+                    ax = plt.subplot(gs[k, n])
+                    ax = disable_ticks(ax)
+                    # Original
+                    if k == 0:
+                        ax.imshow(x[n, :][0], cmap="binary", vmin=0, vmax=1)
+                    # Mean
+                    elif k == 1:
+                        ax.imshow(x_mean[n, :][0], cmap="binary", vmin=0, vmax=1)
+                    # Variance
+                    elif k == 2:
+                        ax.imshow(x_var[n, :][0], cmap="binary")
+                    # Sample
+                    elif k == 3:
+                        ax.imshow(x_hat[n, :][0], cmap="binary", vmin=0, vmax=1)
 
             str_title = f"{pl_module.__class__.__name__}_images"
             trainer.logger.experiment.add_image(
-                str_title, grid, global_step=trainer.global_step, dataformats="CHW"
+                str_title,
+                plot_to_image(fig),
+                global_step=trainer.global_step,
+                dataformats="CHW",
             )
