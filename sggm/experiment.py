cfbcbcfc132e7595758bf9f36241c63c525dfc9a
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-03-26 14:53:38 +0100

    Useless stuff. ciao.

--- sggm/experiment.py
+++ sggm/experiment.py
@@ -388,13 +388,6 @@ def cli_main():
             datamodule = experiment.datamodule
             datamodule.setup()
 
-            shift_investigation = False
-            if shift_investigation:
-                print(
-                    f"{len(datamodule.train_dataset)};{len(datamodule.val_dataset)};{len(datamodule.test_dataset)}"
-                )
-                continue
-
             # ------------
             # model
             # ------------
