5ee5eaaac5d89464844821a639f6ce9705388873
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-04-01 11:42:57 +0200

    Have i finally understood.

--- sggm/vae_model.py
+++ sggm/vae_model.py
@@ -572,7 +572,7 @@ class V3AE(BaseVAE):
                 prior_modes, max_mode * torch.ones_like(prior_modes)
             )
             self.prior_β = self.prior_b * torch.ones_like(prior_modes).type_as(x_train)
-            self.prior_α = (1 + self.decoder_α_offset) + self.prior_β * prior_modes
+            self.prior_α = 1 + self.prior_β * prior_modes
 
         elif (prior_α is not None) & (prior_β is not None):
             assert type(prior_α) == type(
@@ -592,6 +592,16 @@ class V3AE(BaseVAE):
                 self.prior_β = prior_β
         else:
             raise ValueError("Incorrect prior values provided, prior_α and prior_β")
+
+        #%
+        # import matplotlib.pyplot as plt
+
+        # fig, (ax_alpha, ax_beta, ax_uncertainty) = plt.subplots(1, 3)
+        # ax_alpha.imshow(self.prior_α, cmap="binary")
+        # ax_beta.imshow(self.prior_β, cmap="binary")
+        # ax_uncertainty.imshow(self.prior_β / (self.prior_α - 1))
+        # plt.show()
+        # exit()
 
     def _setup_pi_dl(self):
         """ Generate a pseudo-input dataloader """
