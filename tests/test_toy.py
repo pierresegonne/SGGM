0675340db34d2eee13c9e11c820f0894f17d5868
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-05-02 15:51:03 +0200

    Fix test.

--- tests/test_toy.py
+++ tests/test_toy.py
@@ -7,7 +7,9 @@ from sggm.definitions import EVAL_LOSS, F_SIGMOID, TEST_LOSS
 def test_toy():
 
     seed_everything(123)
-    model = VariationalRegressor(input_dim=1, hidden_dim=50, output_dim=1, activation=F_SIGMOID)
+    model = VariationalRegressor(
+        input_dim=1, hidden_dim=50, out_dim=1, activation=F_SIGMOID
+    )
     datamodule = ToyDataModule(128, 0)
     trainer = Trainer(max_epochs=5)
     trainer.fit(model, datamodule)
