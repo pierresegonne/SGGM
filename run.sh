c10c2edbe5f3cce8a4164553cd22bf6f0ce9e89b
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-05-02 20:18:50 +0200

    Attempt with cuda.

--- run.sh
+++ run.sh
@@ -37,4 +37,5 @@ python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
 export PYTHONIOENCODING=utf8
-cd sggm/ && python experiment.py --experiments_config configs/mnist_2d_latent_space_rv3ae.yml --gpus -1
+# cd sggm/ && python experiment.py --experiments_config configs/mnist_2d_latent_space_rv3ae.yml --gpus -1
+cd sggm/ && python baselines/run.py --model john --experiment_name toy --n_trials 1 --iters 100
