86ab8d3c452a6dca16aca13b56548a59741f5958
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-05-03 22:47:54 +0200

    Run on all uci, all baselines.

--- run.sh
+++ run.sh
@@ -38,4 +38,4 @@ source env/bin/activate
 python3 -m pip install -r requirements.txt
 export PYTHONIOENCODING=utf8
 # cd sggm/ && python experiment.py --experiments_config configs/mnist_2d_latent_space_rv3ae.yml --gpus -1
-cd sggm/ && python baselines/run.py --experiment_name uci_concrete --n_trials 4
+cd sggm/ && python baselines/run.py --n_trials 20
