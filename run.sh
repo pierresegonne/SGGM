a5d8500d1f1f8fb11b4c9cc7cdc95f1dbfa18897
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-05-05 22:19:29 +0200

    Run file.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ### specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J mnist_2d_latent_space_rv3ae
+#BSUB -J benchmarks_shifted_split
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
@@ -37,5 +37,7 @@ python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
 export PYTHONIOENCODING=utf8
-# cd sggm/ && python experiment.py --experiments_config configs/mnist_2d_latent_space_rv3ae.yml --gpus -1
-cd sggm/ && python baselines/run.py --experiment_name uci_carbon --n_trials 20
+# Our model run
+cd sggm/ && python experiment.py --experiments_config configs/benchmarks_shifted_split.yml --gpus -1
+# Baselines run
+# cd sggm/ && python baselines/run.py --experiment_name uci_carbon --n_trials 20
